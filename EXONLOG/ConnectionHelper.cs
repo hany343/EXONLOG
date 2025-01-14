2b0bb2a481d563029b4258f887240b70c8bcf320
Hany ragab <h.ragab611@gmail.com>
2025-01-14 21:00:59 +0200

    Connectionhelper

---
+++ EXONLOG/ConnectionHelper.cs
@@ -0,0 +1,26 @@
+﻿namespace EXONLOG
+{
+    using Microsoft.Data.SqlClient;
+    using System.Threading.Tasks;
+
+    public static class ConnectionHelper
+    {
+        public static async Task<bool> TestConnectionAsync(string connectionString)
+        {
+            try
+            {
+                using var connection = new SqlConnection(connectionString);
+                await connection.OpenAsync();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+    public class ConnectionStringService
+    {
+        public string ActiveConnectionString { get; set; }
+    }
+}
